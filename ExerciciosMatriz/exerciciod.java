d8022f9323a71a74ab032f9043defbfbd3023d5b
LucasOlyntho <142914126+LucasOlyntho@users.noreply.github.com>
2023-11-21 05:33:48 -0300

    Add files via upload

---
+++ ExerciciosMatriz/exerciciod.java
@@ -0,0 +1,10 @@
+package ListaMatriz;
+import java.util.Random;
+public class exerciciod {
+	public static void main(String args[]) {
+		final int TAM=3;
+		int a[][], i, j, col=0, menor, maior, posilinha, posicoluna;
+		a = new int [TAM][TAM];
+		Random in = new Random();
+}
+}
